9603f7003dbf2d2c75756e8f83ed78ae85ce7763
kumkee <kumkee@users.noreply.github.com>
2025-03-11 06:58:20 +1300

    added lsp servers

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,20 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls" }
+local servers = {
+  "html",
+  "cssls",
+  "ts_ls",
+  "elmls",
+  "fsautocomplete", -- f#
+  "lemminx", -- xml
+  "jsonls", -- json
+  "csharp_ls",
+  "hls", -- Haskell
+  "nil_ls", -- nix
+  "yamlls", -- yaml
+  -- "ltex", -- added below in a separate section
+}
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
@@ -15,6 +28,18 @@ for _, lsp in ipairs(servers) do
     capabilities = nvlsp.capabilities,
   }
 end
+
+-- ltex: special treatment - md, latex, and more
+lspconfig["ltex"].setup {
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+  settings = {
+    ltex = {
+      language = "en-GB",
+    },
+  },
+}
 
 -- configuring single server, example: typescript
 -- lspconfig.ts_ls.setup {
