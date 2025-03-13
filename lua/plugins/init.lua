749e7af6655b4ca1a0c76ecf28fb606e2a99cbe0
kumkee <kumkee@users.noreply.github.com>
2025-03-14 06:46:08 +1300

    added treesitter ensure installs

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,13 +13,19 @@ return {
     end,
   },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "printf",
+        "fsharp",
+        "elm",
+      },
+    },
+  },
 }
