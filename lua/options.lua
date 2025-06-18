aaca5b94adb0ab40697fd956bf01546fc44f7f75
kumkee <kumkee@users.noreply.github.com>
2025-06-19 06:40:37 +1200

    commenting out elixirls in nvim

--- lua/options.lua
+++ lua/options.lua
@@ -1,11 +1,11 @@
 require "nvchad.options"
-require "_nix_elixirls"
+-- require "_nix_elixirls"
 
 -- add yours here!
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
-vim.lsp.config("elixirls", {
+-- vim.lsp.config("elixirls", {
   -- Unix
-  cmd = { Elixirls },
-})
+  -- cmd = { Elixirls },
+-- })
