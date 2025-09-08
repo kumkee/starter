7255d8ee641ad486852b5890d3543ff63b88f169
kumkee <kumkee@users.noreply.github.com>
2025-09-09 06:45:02 +1200

    lazy updates

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,5 +1,7 @@
 require("nvchad.configs.lspconfig").defaults()
 
+local lspconfig = require("lspconfig")
+
 local servers = {
   "html",
   "cssls",
@@ -18,5 +20,15 @@ local servers = {
   "elixirls", -- elixir
 }
 vim.lsp.enable(servers)
+
+lspconfig.elixirls.setup {
+  cmd = { vim.fn.exepath("elixir-ls") }, -- resolves to Nix-provided binary
+  settings = {
+    elixirLS = {
+      dialyzerEnabled = false,   -- optional: faster startup
+      fetchDeps = false,         -- optional: prevents deps auto-fetch
+    },
+  },
+}
 
 -- read :h vim.lsp.config for changing options of lsp servers
