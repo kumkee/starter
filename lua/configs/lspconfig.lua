3ea71c1440ffe38474950891cd992170e6d1f1ba
kumkee <kumkee@users.noreply.github.com>
2025-09-11 07:13:51 +1200

    lsp: + haskell

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -12,7 +12,7 @@ local servers = {
   "lemminx", -- xml
   "jsonls", -- json
   -- "csharp_ls",
-  -- "hls", -- Haskell
+  "hls", -- Haskell
   "nil_ls", -- nix
   "yamlls", -- yaml
   -- "ltex", -- added below in a separate section
@@ -21,14 +21,18 @@ local servers = {
 }
 vim.lsp.enable(servers)
 
-lspconfig.elixirls.setup {
+vim.lsp.config("hls", {
+  filetypes = { "haskell", "lhaskell", "cabal" },
+})
+
+lspconfig.elixirls.setup({
   cmd = { vim.fn.exepath("elixir-ls") }, -- resolves to Nix-provided binary
   settings = {
     elixirLS = {
-      dialyzerEnabled = false,   -- optional: faster startup
-      fetchDeps = false,         -- optional: prevents deps auto-fetch
+      dialyzerEnabled = false, -- optional: faster startup
+      fetchDeps = false, -- optional: prevents deps auto-fetch
     },
   },
-}
+})
 
 -- read :h vim.lsp.config for changing options of lsp servers
